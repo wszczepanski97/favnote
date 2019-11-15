6f507f3d3139d78e9daf9ae8bd2a9ae733377753
Wojciech Szczepanski <w.szczepansk@samsung.com>
2019-11-15 08:05:11 +0100

    Add store created in redux for app

---
+++ src/reducers/index.js
@@ -0,0 +1,37 @@
+const initialState = {
+  twitters: [
+    {
+      id: 1,
+      title: 'Hello Roman',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+      twitterName: 'hello_roman',
+    },
+  ],
+  notes: [
+    {
+      id: 1,
+      title: 'Wake me up when Vue ends',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+  ],
+  articles: [
+    {
+      id: 1,
+      title: 'React on my mind',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    },
+  ],
+};
+
+const rootReducer = (state = initialState, action) => {
+  console.log(action);
+};
+
+export default rootReducer;
