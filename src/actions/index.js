9ccf721c3e5ef5fd470d34e58a1afc95314cc2bc
Wojciech Szczepański <wszczepanski97@gmail.com>
2019-11-24 18:51:20 +0100

    Enable adding items by filling a form

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,21 @@ export const removeItem = (id, itemType) => {
     },
   };
 };
+
+export const addItem = (itemType, itemContent) => {
+  const getId = () =>
+    `_${Math.random()
+      .toString(36)
+      .substr(2, 9)}`;
+
+  return {
+    type: 'ADD_ITEM',
+    payload: {
+      itemType,
+      item: {
+        id: getId(),
+        ...itemContent,
+      },
+    },
+  };
+};
